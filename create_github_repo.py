baa360956093b9642b094b02b09c64a64df83606
abhimanyu98986@gmail.com <Mannu Gupta>
2017-03-02 22:31:50 +0530

    initial commit

---
+++ create_github_repo.py
@@ -0,0 +1,27 @@
+import mechanize
+import cookielib
+
+repo_name = input("Enter the repository name ")
+
+try:
+	br=mechanize.Browser()
+	cj=cookielib.LWPCookieJar()
+	br.set_cookiejar(cj)
+except:
+	print(" Error Generating Browser ")
+
+br.set_handle_robots(False)
+
+r=br.open('http://github.com/login')
+
+br.select_form(nr=0)
+br.form['login']='theparadoxer02'
+br.form['password']='aperchitsinex123'
+br.submit()
+
+r=br.open("http://github.com/new")
+br.select_form(nr=3)
+br.form['repository[name]']=repo_name
+br.form['repository[description]']='made by mechaize'
+
+br.submit()
